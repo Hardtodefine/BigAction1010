4a8b7b44916b442783c44c63ed926f6a9d31926c
Hardtodefine <56499565+Hardtodefine@users.noreply.github.com>
2019-10-15 16:31:16 +0800

    backorigin

---
+++ web_back/js/config.js
@@ -0,0 +1,6 @@
+// config中文含义为配置
+// 我们可以将项目中使用的接口地址统一保存到这个文件中
+var baseurl = 'http://localhost:8000'
+var userLoginPort = baseurl+'/admin/login';
+var userLogoutPort = baseurl+'/admin/logout';
+var userInfoPort = baseurl+'/admin/getuser';
